22b0a30965cba8d1561a4ab398fd0118ec854495
Surraj Shrikaant Khopkar <contact@surrajkhopkar.com>
2025-09-27 12:07:25 +0530

    list

---
+++ Lists/Program1.py
@@ -0,0 +1,14 @@
+lst = []
+print(type(lst))
+list1 = ['suraj','saanvi','priya',1,2,3,4,5]
+print(list1)
+print(list1[0])
+print(list1[3])
+print(list1[-1])
+
+fruits = ['apple','banana','orange','kiwi']
+print(fruits[1:])
+print(fruits[1:3])
+print(fruits[-1:])
+print(fruits[-1:-3])
+print(fruits[-1:3])
---
+++ Lists/Program2.py
@@ -0,0 +1,13 @@
+# Modifying list elements
+
+fruits = ['apple','banana','orange','kiwi']
+fruits.append('pear')
+print(fruits)
+
+fruits[1] = 'mosambi'
+
+print(fruits)
+
+fruits.sort()
+
+print(fruits)
